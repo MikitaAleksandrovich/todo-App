32107f794886be46ae64f15b4ab0d7ed69d64e57
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-07-17 22:01:50 +0400

    Add react element tree and render it on the page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
-function hello() {
-  alert('hello!');
-}
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-hello();
+const el = (
+  <div>
+    <h1>My Todo App</h1>
+    <input placeholder="Search" />
+    <ul>
+      <li>Learn React</li>
+      <li>Build Awesome App</li>
+    </ul>
+  </div>
+);
+
+
+ReactDOM.render(el,
+  document.getElementById('root'));
