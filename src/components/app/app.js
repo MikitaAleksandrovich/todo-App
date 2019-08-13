02538dd5bd23ee4611b21d2e6275c3dea0ee1a86
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-08-13 22:13:43 +0400

    Add functionallity to delete items from state
    Refactor functional stateless component into class Component, cause it must have state to delete and update items.
    Then add all data to state.
    Then add deleteItem method to search id of clicked item and deleted it.
    It's also neccessary to note, that I create new copy of updated Array with all items that is not have deleted items, because there is not allowed to edit excisting state.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { Component } from 'react';
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
@@ -7,30 +6,54 @@ import ItemStatusFilter from '../item-status-filter';
 
 import './app.css';
 
-const App = () => {
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 },
-    { label: 'Do shopping', important: false, id: 4 },
-    { label: 'Make dinner', important: false, id: 5 },
-    { label: 'Go sleep', important: false, id: 6 }
-  ];
- 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
+export default class App extends Component {
+
+  state = {
+    todoData: [
+      { label: 'Drink Coffee', important: false, id: 1 },
+      { label: 'Make Awesome App', important: true, id: 2 },
+      { label: 'Have a lunch', important: false, id: 3 },
+      { label: 'Do shopping', important: false, id: 4 },
+      { label: 'Make dinner', important: false, id: 5 },
+      { label: 'Go sleep', important: false, id: 6 }
+    ]
+  }
+
+  deleteItem = (id) => {
+    this.setState(({ todoData }) => {
+
+      const itemDeletedIndex = todoData.findIndex((el) => el.id === id);
+
+      todoData.splice(itemDeletedIndex, 1);
+
+      const beforeDeltedItem = todoData.slice(0, itemDeletedIndex);
+      const afterDeletedItem = todoData.slice(itemDeletedIndex + 1);
+
+      const newData = [...beforeDeltedItem, ...afterDeletedItem];
+
+      return {
+        todoData: newData
+      }
+
+    });
+  }
+
+  render() {
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={1} done={3} />
+        <div className="top-panel d-flex">
+          <SearchPanel />
+          <ItemStatusFilter />
+        </div>
+  
+        <TodoList 
+          todos={this.state.todoData} 
+          onDeleted={ this.deleteItem }
+        />
       </div>
-
-      <TodoList 
-        todos={todoData} 
-        onDeleted={ (id) => console.log('del', id)}
-      />
-    </div>
-  );
+    );
+  }
+ 
+  
 };
-
-export default App;
