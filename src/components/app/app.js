6d645832ae8f3ef555e90e64a8d414b1264c3fbc
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-08-17 13:03:21 +0400

    Add method onToggleProperty that change property and return new array
    Method to simplify my code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -64,62 +64,46 @@ export default class App extends Component {
     });
   }
 
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex((el) => el.id === id);
+
+    //1. Update Object
+    const oldItem = arr[idx];
+    const newItem = {
+      ...oldItem,
+      [propName]: !oldItem[propName]
+    };
+
+    //2. Update array in state
+    return [
+      ...arr.slice(0, idx),
+      newItem,
+      ...arr.slice(idx + 1)
+    ];
+  }
+
   onToggleImportant = (id) => {
-    console.log('Toggle Important', (id));
-
     this.setState(( {todoData} ) => {
-      const idx = todoData.findIndex((el) => el.id === id);
-
-      //1. Update Object
-      const oldItem = todoData[idx];
-      const newItem = {
-        ...oldItem,
-        important: !oldItem.important
-      };
-
-      //2. Update array in state
-      const newArray = [
-        ...todoData.slice(0, idx),
-        newItem,
-        ...todoData.slice(idx + 1)
-      ];
-
       return {
-        todoData: newArray
+        todoData: this.toggleProperty(todoData, id, 'important')
       };
     });
   };
 
   onToggleDone = (id) => {
     this.setState(( {todoData} ) => {
-      const idx = todoData.findIndex((el) => el.id === id);
-
-      //1. Update Object
-      const oldItem = todoData[idx];
-      const newItem = {
-        ...oldItem,
-        done: !oldItem.done
-      };
-
-      //2. Update array in state
-      const newArray = [
-        ...todoData.slice(0, idx),
-        newItem,
-        ...todoData.slice(idx + 1)
-      ];
-
       return {
-        todoData: newArray
+        todoData: this.toggleProperty(todoData, id, 'done')
       };
     });
   };
 
 
   render() {
+    const { todoData } = this.state;
 
-    const doneCount = this.state.todoData.filter((el) => el.done).length;
-    const todoCount = this.state.todoData.length - doneCount;
-    
+    const doneCount = todoData.filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
 
     return (
       <div className="todo-app">
@@ -131,7 +115,7 @@ export default class App extends Component {
         </div>
   
         <TodoList 
-          todos={this.state.todoData} 
+          todos={ todoData } 
           onDeleted={ this.deleteItem }
           onToggleImportant={ this.onToggleImportant }
           onToggleDone={ this.onToggleDone }
