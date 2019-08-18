b7f530316d7865a1a4080009c8c587ceca19d038
MikitaAleksandrovich <aleksandrovich.mikita@gmail.com>
2019-08-18 17:49:31 +0400

    Add form that allows to enter new item and save it in state

--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -3,17 +3,36 @@ import './item-add-form.css';
 
 
 export default class ItemAddForm extends Component {
+
+    state = {
+        label: ''
+    };
+    
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label);
+    }
+
+
     render() {
-        const { onItemAdded } = this.props;
         return (
-            <div className="item-add-form">
-                <button 
-                    className="btn btn-outline-secondary"
-                    onClick={ () => onItemAdded(`There is new item!`) } 
-                >
+            <form className="item-add-form d-flex"
+                    onSubmit={ this.onSubmit }>
+                <input type="text" 
+                        className="form-control"
+                        onChange={ this.onLabelChange }
+                        placeholder="What need to be done" 
+                        />
+                <button className="btn btn-outline-secondary">
                     Add Item
                 </button>
-            </div>
+            </form>
         );
     }
 };
